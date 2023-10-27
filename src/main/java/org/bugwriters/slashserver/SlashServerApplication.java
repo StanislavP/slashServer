eca0a856580c3d559d1862746c3a320189837edc
Stanislav Pelov <stanislav.pelov@gmail.com>
2023-10-27 21:32:46 +0300

    initial commit

---
+++ src/main/java/org/bugwriters/slashserver/SlashServerApplication.java
@@ -0,0 +1,13 @@
+package org.bugwriters.slashserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlashServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SlashServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/bugwriters/slashserver/SlashServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.bugwriters.slashserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlashServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
